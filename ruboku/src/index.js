f53a4300280ba2759347bd51776f949b42852d18
huw1990 <huwcarpenter@gmail.com>
2019-01-13 20:10:56 +0000

    Split various game component into separate classes, ready to add state for the game functionality

--- ruboku/src/index.js
+++ ruboku/src/index.js
@@ -2,49 +2,113 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Numbers extends React.Component {
+
+  renderNumber(i) {
+    return (
+      <Number
+        value={i}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div className="number-buttons">
+        {this.renderNumber(1)}
+        {this.renderNumber(2)}
+        {this.renderNumber(3)}
+        {this.renderNumber(4)}
+        {this.renderNumber(5)}
+        {this.renderNumber(6)}
+        {this.renderNumber(7)}
+        {this.renderNumber(8)}
+        {this.renderNumber(9)}
+        {this.renderNumber(10)}
+      </div>
+    );
+  }
+}
+
+function Number(props) {
+  return (
+    <div
+      className="number-button-item"
+      id="clickable">
+      {props.value}
+    </div>
+  );
+}
+
+class Operations extends React.Component {
+  renderOperation(val) {
+    return (
+      <Operation
+        value={val}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div className="op-buttons">
+        {this.renderOperation("+")}
+        {this.renderOperation("-")}
+        {this.renderOperation("x")}
+        {this.renderOperation("/")}
+      </div>
+    );
+  }
+}
+
+function Operation(props) {
+  return (
+    <div
+      className="op-button-item"
+      id="clickable">
+      {props.value}
+    </div>
+  );
+}
+
+class Grid extends React.Component {
+
+  //TODO: have proper state, separate class for each item in the grid
+  render() {
+    return (
+      <div className="game-grid">
+        <div className="game-item" id="emptygrid"> </div>
+        <div className="game-item" id="gameclickable">v</div>
+        <div className="game-item" id="gameclickable">v</div>
+        <div className="game-item" id="gameclickable">v</div>
+        <div className="game-item" id="gameclickable">></div>
+        <div className="game-item">1</div>
+        <div className="game-item">1</div>
+        <div className="game-item">1</div>
+        <div className="game-item" id="gameclickable">></div>
+        <div className="game-item">1</div>
+        <div className="game-item">1</div>
+        <div className="game-item">1</div>
+        <div className="game-item" id="gameclickable">></div>
+        <div className="game-item">1</div>
+        <div className="game-item">1</div>
+        <div className="game-item">1</div>
+    </div>
+    );
+  }
+}
+
 class Game extends React.Component {
+
   render() {
     return (
       <div className="container">
         <h1>Ruboku</h1>
         <p>Moves: 0</p>
         <div className="undo-button">Undo Last Move</div>
-        <div className="game-grid">
-          <div className="game-item" id="emptygrid"> </div>
-          <div className="game-item" id="gameclickable">v</div>
-          <div className="game-item" id="gameclickable">v</div>
-          <div className="game-item" id="gameclickable">v</div>
-          <div className="game-item" id="gameclickable">></div>
-          <div className="game-item">1</div>
-          <div className="game-item">1</div>
-          <div className="game-item">1</div>
-          <div className="game-item" id="gameclickable">></div>
-          <div className="game-item">1</div>
-          <div className="game-item">1</div>
-          <div className="game-item">1</div>
-          <div className="game-item" id="gameclickable">></div>
-          <div className="game-item">1</div>
-          <div className="game-item">1</div>
-          <div className="game-item">1</div>
-        </div>
-        <div className="number-buttons">
-          <div className="number-button-item" id="clickable">1</div>
-          <div className="number-button-item" id="clickable">2</div>
-          <div className="number-button-item" id="clickable">3</div>
-          <div className="number-button-item" id="clickable">4</div>
-          <div className="number-button-item" id="clickable">5</div>
-          <div className="number-button-item" id="clickable">6</div>
-          <div className="number-button-item" id="clickable">7</div>
-          <div className="number-button-item" id="clickable">8</div>
-          <div className="number-button-item" id="clickable">9</div>
-          <div className="number-button-item" id="clickable">10</div>
-        </div>
-        <div className="op-buttons">
-          <div className="op-button-item" id="clickable">+</div>
-          <div className="op-button-item" id="clickable">-</div>
-          <div className="op-button-item" id="clickable">x</div>
-          <div className="op-button-item" id="clickable">/</div>
-        </div>
+        <Grid/>
+        <Numbers/>
+        <Operations/>
       </div>
     );
   }
